9d737e6bcd98357df49b9e77e3dbea6a36664184
Polete <pbusquets-dam@pbusquets.dam@institutcampalans.net>
2025-10-01 19:16:46 +0200

    segon commit

--- sessio1/MainWindow.xaml.cs
+++ sessio1/MainWindow.xaml.cs
@@ -23,11 +23,35 @@ namespace sessio1
         public MainWindow()
         {
             InitializeComponent();
+
+            Grid elmeugrid = new Grid();
+            this.Content = elmeugrid;
+            Button btn = new Button();
+            //elmeugrid.Children.Add(btn);
+            WrapPanel elmeuWrapPanel = new WrapPanel();
+
+            TextBlock txt = new TextBlock();
+            txt.Text = "hola ";
+            elmeuWrapPanel.Children.Add(txt);
+            txt.Foreground = Brushes.AliceBlue;
+
+
+            TextBlock txt2 = new TextBlock();
+            txt2.Text = "adeu ";
+            elmeuWrapPanel.Children.Add(txt2);
+            txt2.Foreground = Brushes.HotPink;
+
+            TextBlock txt3 = new TextBlock();
+            txt3.Text = "andreu ";
+            elmeuWrapPanel.Children.Add(txt3);
+            txt3.Foreground = Brushes.Gold;
+
+            btn.Height = 100;
+            btn.Width = 200;
+            btn.Content = elmeuWrapPanel;
+            elmeugrid.Children.Add(btn);
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
-        }
     }
 }
