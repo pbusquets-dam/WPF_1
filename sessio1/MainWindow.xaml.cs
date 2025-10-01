db90d7df5e67b2867417de447ff8f8f97f37b6d7
Pol Busquets <pbusquets-dam@pbusquets.dam@institutcampalans.net>
2025-10-01 18:34:40 +0200

    segon commit

--- sessio1/MainWindow.xaml.cs
+++ sessio1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace sessio1
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
